785c010bb4b052f2dbe95086665be4022ff7b8a5
peihan <D1051857@o365.fcu,edu.tw>
2022-11-28 10:54:29 +0800

    add a random message

--- src/main/java/com/github/peihan0/Main.java
+++ src/main/java/com/github/peihan0/Main.java
@@ -2,6 +2,9 @@ package com.github.peihan0;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello Java!");
         System.out.println("Hi world!");
+
+        System.out.println("TESTING");
     }
 }
